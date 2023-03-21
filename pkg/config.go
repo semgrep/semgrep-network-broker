4a96e4b3fc9e3d3a525a7b57981feff833af00b3
Tom Petr <tom@r2c.dev>
2023-03-21 07:56:33 -0400

    add config option to force http (#22)

--- pkg/config.go
+++ pkg/config.go
@@ -190,12 +190,17 @@ type HeartbeatConfig struct {
 	FirstHeartbeatMustSucceed bool   `mapstructure:"firstHeartbeatMustSucceed" json:"firstHeartbeatMustSucceed"`
 }
 
+type UnsafeConfig struct {
+	ForceHTTP bool `mapstructure:"forceHttp" json:"forceHttp"`
+}
+
 type InboundProxyConfig struct {
 	Wireguard       WireguardBase   `mapstructure:"wireguard" json:"wireguard"`
 	Allowlist       Allowlist       `mapstructure:"allowlist" json:"allowlist"`
 	ProxyListenPort int             `mapstructure:"proxyListenPort" json:"proxyListenPort" validate:"gte=0" default:"80"`
 	Logging         LoggingConfig   `mapstructure:"logging" json:"logging"`
 	Heartbeat       HeartbeatConfig `mapstructure:"heartbeat" json:"heartbeat"`
+	Unsafe          UnsafeConfig    `mapstructure:"unsafe" json:"unsafe"`
 }
 
 type Config struct {
