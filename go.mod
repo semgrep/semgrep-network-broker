118b953f059beb0dda3bf2b9004387b512c49b15
Vivek Khimani <vivek@r2c.dev>
2024-05-13 14:46:23 -0700

    redact tokens from logs

--- go.mod
+++ go.mod
@@ -59,6 +59,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/whuang8/redactrus v1.0.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-import "github.com/semgrep/semgrep-network-broker/cmd"
+import (
+	"github.com/semgrep/semgrep-network-broker/cmd"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/whuang8/redactrus"
+)
+
+
+func init() {
+	// Create Redactrus hook that is triggered
+	// for every logger level and redacts
+	// github oauth tokens from logs
+	// regex source: https://gist.github.com/magnetikonline/073afe7909ffdd6f10ef06a00bc3bc88
+	rh := &redactrus.Hook{
+			AcceptedLevels: log.AllLevels,
+			RedactionList:  []string{"^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$"},
+	}
+
+	log.AddHook(rh)
+}
 
 func main() {
 	cmd.Execute()
