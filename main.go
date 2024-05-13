a5880274f5c55ff3d976294e0fbd19a2c70852c6
Vivek Khimani <vivek@r2c.dev>
2024-05-13 15:19:42 -0700

    regex

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func init() {
 	// regex source: https://gist.github.com/magnetikonline/073afe7909ffdd6f10ef06a00bc3bc88
 	rh := &redactrus.Hook{
 			AcceptedLevels: log.AllLevels,
-			RedactionList:  []string{"^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$"},
+			RedactionList:  []string{"(oauth2:)^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$(@)"},
 	}
 
 	log.AddHook(rh)
