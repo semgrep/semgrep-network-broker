a033ee51822325328712ecb4f867a5ea3bacaf08
Vivek Khimani <vivek@semgrep.com>
2024-06-28 13:39:59 -0700

    Vivek/pre commit (#76)
    * trying pre-commit
    
    * trying pre-commit
    
    * trying pre-commit
    
    * trying pre-commit
    
    * trying pre-commit
    
    * pre-commit

--- main.go
+++ main.go
@@ -7,15 +7,14 @@ import (
 	"github.com/whuang8/redactrus"
 )
 
-
 func init() {
 	// Create Redactrus hook that is triggered
 	// for every logger level and redacts
 	// github oauth tokens from logs
 	// regex source: https://gist.github.com/magnetikonline/073afe7909ffdd6f10ef06a00bc3bc88
 	rh := &redactrus.Hook{
-			AcceptedLevels: log.AllLevels,
-			RedactionList:  []string{"(oauth2:)gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59}(@)"},
+		AcceptedLevels: log.AllLevels,
+		RedactionList:  []string{"(oauth2:)gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59}(@)"},
 	}
 
 	log.AddHook(rh)
